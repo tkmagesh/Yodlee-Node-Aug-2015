25419313250264269bf6d158510fd62d3889f797
tkmagesh <tkmagesh77@gmail.com>
2015-08-20 16:29:10 +0530

    app server - seed

--- Day-01/07-webServer/server.js
+++ Day-01/07-webServer/server.js
@@ -2,15 +2,33 @@ var http = require('http');
 var path = require('path');
 var fs = require('fs');
 
+var staticResourceExtns = ['.html','.css','.js','.png','.ico','.jpg','.xml','.json'];
+
+function isStatic(resource){
+    return staticResourceExtns.indexOf(path.extname(resource)) !== -1;
+}
+
 var server = http.createServer(function(req, res){
-    var resourceUrl = req.url === '/' ? '/index.html' : req.url;
-    var resourcePath = path.join(__dirname, resourceUrl);
-    if (!fs.existsSync(resourcePath)){
+    /*
+    /calculator?operation=add&n1=100&n2=200
+    */
+    req.url = url.parse(req.url, true);
+    if (isStatic(req.url.pathname)){
+        var resourceUrl = req.url.pathname === '/' ? '/index.html' : req.url.pathname;
+
+        var resourcePath = path.join(__dirname, resourceUrl);
+        if (!fs.existsSync(resourcePath)){
+            res.statusCode = 404;
+            res.end();
+            return;
+        }
+        fs.createReadStream(resourcePath).pipe(res);
+    } else if (req.url.pathname === '/calculator'){
+
+    } else {
         res.statusCode = 404;
         res.end();
-        return;
     }
-    fs.createReadStream(resourcePath).pipe(res);
 });
 server.listen(8080);
 console.log("server listening on port 8080");
