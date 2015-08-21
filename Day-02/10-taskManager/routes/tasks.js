7be1354241b0a90d6b813b9203c1824511aad6b4
tkmagesh <tkmagesh77@gmail.com>
2015-08-21 15:12:58 +0530

    Toggling completion status

--- Day-02/10-taskManager/routes/tasks.js
+++ Day-02/10-taskManager/routes/tasks.js
@@ -2,13 +2,46 @@ var express = require('express');
 var router = express.Router();
 
 var taskList = [
-    'Watch a movie',
-    'Go shopping',
-    'Fix that bug'
+     { id : 1, name : 'Watch a movie', isCompleted : false},
+     { id : 2, name : 'Go shopping', isCompleted : false},
+     { id : 3, name : 'Fix that bug', isCompleted : false},
 ];
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('tasks/index', {list : taskList});
+    var viewModel = {
+        list : taskList,
+        total : taskList.length,
+        completed : taskList.filter(function(t){ return t.isCompleted }).length
+    };
+  res.render('tasks/index', viewModel);
+});
+
+router.get('/new', function(req, res, next){
+    res.render('tasks/new');
+});
+
+router.post('/new', function(req, res, next){
+    var newTaskId = taskList.reduce(function(result, task){
+        return result > task.id ? result : task.id;
+    },0) + 1;
+    var newTask = {
+        id : newTaskId,
+        name : req.body.newTaskName,
+        isCompleted : false
+    };
+    taskList.push(newTask);
+    res.redirect('/tasks');
+});
+
+router.get('/toggle/:id', function(req, res, next){
+    var taskId = parseInt(req.params.id);
+    var task = taskList.filter(function(t){
+        return t.id === taskId;
+    })[0];
+    if (task){
+        task.isCompleted = !task.isCompleted;
+    }
+    res.redirect('/tasks');
 });
 
 module.exports = router;
